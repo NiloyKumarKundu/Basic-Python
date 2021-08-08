4b7894086633ba2bee1ad468292ce693e711cb40
Niloy <nkundu191071@gmail.com>
2021-08-08 21:32:45 +0600

    Problem Solving

---
+++ test.py
@@ -0,0 +1,26 @@
+a = 1
+print(type(a))
+a = 1.0
+print(type(a))
+a = "abcd"
+print(type(a))
+
+# Build In default data type
+str1 = "Hello"
+print(type(str1))
+a = 1
+b = 1.00
+c = 2j      # complex
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Sequential Data type
+list1 = [a, "hello", 1.0]   # List type
+t = (a, "hello", 1, 12.0)   # touple type
+
+dict = {                    # dictionary type
+    "key1": 1,
+    "key2": "hello"
+}
+
